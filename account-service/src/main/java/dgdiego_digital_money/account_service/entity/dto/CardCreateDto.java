6d346fab6f37580be087c334f5c56bc9f92ffd92
dgdiego <dgdiego.gimenez@gmail.com>
2025-10-06 12:09:12 -0300

    Validar número tarjeta al crearla + Actualizando postman collection

--- account-service/src/main/java/dgdiego_digital_money/account_service/entity/dto/CardCreateDto.java
+++ account-service/src/main/java/dgdiego_digital_money/account_service/entity/dto/CardCreateDto.java
@@ -2,6 +2,7 @@ package dgdiego_digital_money.account_service.entity.dto;
 
 import dgdiego_digital_money.account_service.entity.domian.CardType;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,6 +18,7 @@ import java.time.LocalDate;
 public class CardCreateDto {
 
     @NotNull
+    @Pattern(regexp = "\\d{4}-\\d{4}-\\d{4}-\\d{4}", message = "El número de tarjeta debe tener el formato ####-####-####-####")
     private String number;
 
     @NotNull
